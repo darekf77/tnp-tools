a323ea14bc46bbe7f296bcce6499b7e62ff584b5
Dariusz Filipiak <dariusz.filipiak@igt.com>
2021-05-24 09:18:39 +0200

    a

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { CLASS } from 'typescript-class-helpers';
 import { Log, Level } from 'ng2-logger';
 import axios, { AxiosResponse } from 'axios';
 import { walk } from 'lodash-walk-object';
-import * as json5 from 'json5';
+
 import * as stringify from 'json-stringify-safe';
 import { JSON10, Circ } from 'json10';
 import { diffChars } from 'diff';
@@ -35,18 +35,13 @@ import * as io from 'socket.io';
 import * as ioClient from 'socket.io-client';
 
 //#region @backend
-import * as chokidar from 'chokidar';
+
 import { IncCompiler } from 'incremental-compiler';
 import * as tsfmt from 'typescript-formatter';
-import { URL } from 'url';
+
 import { describe, before, beforeEach, it } from 'mocha';
 import { expect } from 'chai';
-import * as glob from 'glob';
-import * as ps from 'ps-node';
-import * as fkill from 'fkill';
-import * as ncp from 'copy-paste';
 import * as Task from 'task.js';
-import isElevated from 'is-elevated';
 
 // CLI UI
 import * as inquirer from 'inquirer'
@@ -70,7 +65,7 @@ import * as crypto from 'crypto';
 import { v4 as uuidv4 } from 'uuid';
 
 // typeorm
-import * as tsorm from 'typeorm'
+import * as tsorm from 'typeorm';
 import {
   Repository as TypeormRepository, EntityRepository
 } from 'typeorm';
