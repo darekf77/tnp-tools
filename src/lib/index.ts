0c0de69a846ef314c373fa66152da40cb824565b
Dariusz Filipiak <darekf77@gmail.com>
2022-05-05 18:38:25 +0100

    things for tests

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,97 @@
-export function helloWorldFromTnpTools() { }
-        
+export * from 'tnp-core';
+import * as dateformat from 'dateformat';
+import { CLASS } from 'typescript-class-helpers';
+
+import axios, { AxiosResponse } from 'axios';
+import { walk } from 'lodash-walk-object';
+console.log('super')
+
+//#region @browser
+import * as fuzzy from 'fuzzy';
+//#endregion
+import { Log } from 'ng2-logger';
+Log.create;
+
+
+// @browserLine
+import { Observable } from 'rxjs';
+
+
+
+import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+import { Subscription } from 'rxjs';
+
+
+new Observable();
+new Subject
+fuzzy.filter
+
+
+import * as stringify from 'json-stringify-safe';
+import { JSON10, Circ } from 'json10';
+import { diffChars } from 'diff';
+
+
+// anuglar
+
+import {
+  ViewChild, ViewContainerRef, OnDestroy,
+  OnInit, Component, Input, AfterViewInit,
+  EventEmitter,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup
+} from '@angular/forms';
+import { NavigationEnd, Router } from '@angular/router';
+import { FieldWrapper, FieldType, FormlyFieldConfig } from '@ngx-formly/core';
+
+
+// sockets
+import { Socket } from 'socket.io';
+import { Server, Namespace } from 'socket.io';
+import * as io from 'socket.io';
+import * as ioClient from 'socket.io-client';
+
+//#region @backend
+
+import { IncCompiler } from 'incremental-compiler';
+import * as tsfmt from 'typescript-formatter';
+
+import { describe, before, beforeEach, it } from 'mocha';
+import { expect } from 'chai';
+import * as Task from 'task.js';
+
+// CLI UI
+import * as inquirer from 'inquirer'
+import * as inquirerAutocomplete from 'inquirer-autocomplete-prompt';
+inquirer.registerPrompt('autocomplete', inquirerAutocomplete)
+
+// express
+import * as express from 'express';
+import * as httpProxy from 'http-proxy';
+import { Http2Server } from 'http2';
+import * as  cors from 'cors';
+import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+import * as methodOverride from 'method-override';
+import * as fileUpload from 'express-fileupload';
+
+// database
+import { generate } from 'password-hash';
+import * as pass from 'passport';
+import * as crypto from 'crypto';
+import { v4 as uuidv4 } from 'uuid';
+
+// typeorm
+import * as tsorm from 'typeorm';
+import {
+  Repository as TypeormRepository, EntityRepository
+} from 'typeorm';
+import { Repository } from 'typeorm';
+import { Connection } from 'typeorm';
+import { EventSubscriber } from 'typeorm';
+import { createConnections, getConnection } from 'typeorm';
+
+//#endregion
