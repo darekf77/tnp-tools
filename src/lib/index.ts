3865977329e34479ee1c01cbc2e08bef9f62f0a5
Dariusz Filipiak <dariusz.filipiak@igt.com>
2022-10-01 17:44:21 +0200

    new version 0.0.58

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,97 +1,103 @@
-export * from 'tnp-core';
-import * as dateformat from 'dateformat';
-import { CLASS } from 'typescript-class-helpers';
 
-import axios, { AxiosResponse } from 'axios';
-import { walk } from 'lodash-walk-object';
-console.log('super')
+export function helloworldsssadasdasd() {
 
-//#region @browser
-import * as fuzzy from 'fuzzy';
-//#endregion
-import { Log } from 'ng2-logger';
-Log.create;
+}
 
+export default helloworldsssadasdasd;
+// export * from 'tnp-core';
+// import * as dateformat from 'dateformat';
+// import { CLASS } from 'typescript-class-helpers';
 
-// @browserLine
-import { Observable } from 'rxjs';
+// import axios, { AxiosResponse } from 'axios';
+// import { walk } from 'lodash-walk-object';
+// console.log('super')
 
-
-
-import { Subject } from 'rxjs';
-import { BehaviorSubject } from 'rxjs';
-import { Subscription } from 'rxjs';
+// //#region @browser
+// import * as fuzzy from 'fuzzy';
+// //#endregion
+// import { Log } from 'ng2-logger';
+// Log.create;
 
 
-new Observable();
-new Subject
-fuzzy.filter
+// // @browserLine
+// import { Observable } from 'rxjs';
 
 
-import * as stringify from 'json-stringify-safe';
-import { JSON10, Circ } from 'json10';
-import { diffChars } from 'diff';
+
+// import { Subject } from 'rxjs';
+// import { BehaviorSubject } from 'rxjs';
+// import { Subscription } from 'rxjs';
 
 
-// anuglar
-
-import {
-  ViewChild, ViewContainerRef, OnDestroy,
-  OnInit, Component, Input, AfterViewInit,
-  EventEmitter,
-} from '@angular/core';
-import {
-  FormControl,
-  FormGroup
-} from '@angular/forms';
-import { NavigationEnd, Router } from '@angular/router';
-import { FieldWrapper, FieldType, FormlyFieldConfig } from '@ngx-formly/core';
+// new Observable();
+// new Subject
+// fuzzy.filter
 
 
-// sockets
-import { Socket } from 'socket.io';
-import { Server, Namespace } from 'socket.io';
-import * as io from 'socket.io';
-import * as ioClient from 'socket.io-client';
+// import * as stringify from 'json-stringify-safe';
+// import { JSON10, Circ } from 'json10';
+// import { diffChars } from 'diff';
 
-//#region @backend
 
-import { IncCompiler } from 'incremental-compiler';
-import * as tsfmt from 'typescript-formatter';
+// // anuglar
 
-import { describe, before, beforeEach, it } from 'mocha';
-import { expect } from 'chai';
-import * as Task from 'task.js';
+// import {
+//   ViewChild, ViewContainerRef, OnDestroy,
+//   OnInit, Component, Input, AfterViewInit,
+//   EventEmitter,
+// } from '@angular/core';
+// import {
+//   FormControl,
+//   FormGroup
+// } from '@angular/forms';
+// import { NavigationEnd, Router } from '@angular/router';
+// import { FieldWrapper, FieldType, FormlyFieldConfig } from '@ngx-formly/core';
 
-// CLI UI
-import * as inquirer from 'inquirer'
-import * as inquirerAutocomplete from 'inquirer-autocomplete-prompt';
-inquirer.registerPrompt('autocomplete', inquirerAutocomplete)
 
-// express
-import * as express from 'express';
-import * as httpProxy from 'http-proxy';
-import { Http2Server } from 'http2';
-import * as  cors from 'cors';
-import * as bodyParser from 'body-parser';
-import * as cookieParser from 'cookie-parser';
-import * as methodOverride from 'method-override';
-import * as fileUpload from 'express-fileupload';
+// // sockets
+// import { Socket } from 'socket.io';
+// import { Server, Namespace } from 'socket.io';
+// import * as io from 'socket.io';
+// import * as ioClient from 'socket.io-client';
 
-// database
-import { generate } from 'password-hash';
-import * as pass from 'passport';
-import * as crypto from 'crypto';
-import { v4 as uuidv4 } from 'uuid';
+// //#region @backend
 
-// typeorm
-import * as tsorm from 'typeorm';
-import {
-  Repository as TypeormRepository, EntityRepository
-} from 'typeorm';
-import { Repository } from 'typeorm';
-import { Connection } from 'typeorm';
-import { EventSubscriber } from 'typeorm';
-import { createConnections, getConnection } from 'typeorm';
+// import { IncCompiler } from 'incremental-compiler';
+// import * as tsfmt from 'typescript-formatter';
 
-//#endregion
+// import { describe, before, beforeEach, it } from 'mocha';
+// import { expect } from 'chai';
+// import * as Task from 'task.js';
+
+// // CLI UI
+// import * as inquirer from 'inquirer'
+// import * as inquirerAutocomplete from 'inquirer-autocomplete-prompt';
+// inquirer.registerPrompt('autocomplete', inquirerAutocomplete)
+
+// // express
+// import * as express from 'express';
+// import * as httpProxy from 'http-proxy';
+// import { Http2Server } from 'http2';
+// import * as  cors from 'cors';
+// import * as bodyParser from 'body-parser';
+// import * as cookieParser from 'cookie-parser';
+// import * as methodOverride from 'method-override';
+// import * as fileUpload from 'express-fileupload';
+
+// // database
+// import { generate } from 'password-hash';
+// import * as pass from 'passport';
+// import * as crypto from 'crypto';
+// import { v4 as uuidv4 } from 'uuid';
+
+// // typeorm
+// import * as tsorm from 'typeorm';
+// import {
+//   Repository as TypeormRepository, EntityRepository
+// } from 'typeorm';
+// import { Repository } from 'typeorm';
+// import { Connection } from 'typeorm';
+// import { EventSubscriber } from 'typeorm';
+// import { createConnections, getConnection } from 'typeorm';
+
+// //#endregion
